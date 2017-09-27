3496df530735c5d487e675109c21a8f3a37f2c74
l4v <ivos.jovan@gmail.com>
2017-09-27 02:26:03 +0200

    ADDED WORLD, WORLD I/O INTO .TXT

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -4,16 +4,19 @@ import entities.creatures.Player;
 import gfx.Assets;
 import main.Game;
 import tiles.Tile;
+import worlds.World;
 
 import java.awt.*;
 
 public class GameState extends State {
 
     private Player player;
+    private World world;
 
     public GameState(Game game){
         super(game);
         player = new Player(game, 100, 100);
+        world = new World("res/worlds/world_0.txt");
 
     }
     @Override
@@ -23,7 +26,7 @@ public class GameState extends State {
     }
     @Override
     public void render(Graphics g){
+        world.render(g);
         player.render(g);
-        Tile.tiles[1].render(g, 0,0);
     }
 }
